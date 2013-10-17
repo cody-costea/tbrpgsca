80546dd7b4aa7843e6777f456769144ad5211950
cody.stephen.costea <cody.stephen.costea@8f547ee5-703d-da68-5a24-46953a7c698d>
2013-10-17 10:22:42 +0000

    updating

--- trunk/tbrpgsca/src/com/tbrpgsca/library/Race.java
+++ trunk/tbrpgsca/src/com/tbrpgsca/library/Race.java
@@ -3,14 +3,16 @@
  */
 package com.tbrpgsca.library;
 
+import java.util.ArrayList;
+
 public class Race {
 	public String rname;
 	public int maxhp,maxmp,maxsp,matk,mdef,mwis,mspi,magi;
 	public int resm[]={0,0,0,0,0,0,0,0};
+    public ArrayList<Integer> skill = new ArrayList<Integer>();
 	
 	public Race(){
-		this.rname="Humanoid";
-		this.raceStats(25, 25, 25, 10, 10, 10, 10, 10);
+		this("Humanoid", 25, 25, 25, 10, 10, 10, 10, 10);
 	}
 	
 	public Race(String rname, int maxhp, int maxmp, int maxsp, int atk,
