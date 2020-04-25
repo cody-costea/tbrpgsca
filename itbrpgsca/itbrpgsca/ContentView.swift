b86ded447b6eef6fd9f8a63d175fb7a370b30a34
Claudiu Costea <cody.costea@yahoo.com>
2020-04-25 17:33:34 +0300

    initial commit

---
+++ itbrpgsca/itbrpgsca/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  itbrpgsca
+//
+//  Created by Costea Claudiu Stefan on 25/04/2020.
+//  Copyright © 2020 Costea Claudiu Stefan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
